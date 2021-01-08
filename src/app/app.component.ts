5bddb3b797bee2e0a63c5c0c46dcf765ad29bb9b
Brian.Treese <brian.treese@socreate.it>
2021-01-07 22:55:31 -0800

    add module-02-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'saa-app',
+  encapsulation: ViewEncapsulation.None,
   template: `
     <header>
       <h1>Styling Angular Applications</h1>
@@ -10,7 +11,24 @@ import { Component } from '@angular/core';
     <div>
       <saa-app-nav></saa-app-nav>
     </div>
-  `
+  `,
+  styles: [`
+
+    body {
+      color: #666;
+      font-family: arial, sans-serif;
+      margin: 0;
+      padding: 1.5em;
+    }
+
+    h1 {
+      color: #2A9FBC;
+      font-size: 200%;
+      line-height: 1;
+      margin: 0.5em 0 0.25em;
+    }
+
+  `]
 })
 
 export class AppComponent {
