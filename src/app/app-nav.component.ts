f9551a21e2f9e6b1f3976b40f868040887ba66bd
Brian.Treese <brian.treese@socreate.it>
2021-01-10 22:20:32 -0800

    add module-02-08

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -3,31 +3,24 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'saa-app-nav',
     template: `
-        <div style="background: #2A9FBC; border-radius: 0.5em; margin: 1.5em 0; padding: 1em 1.2em;">
-            <ul style="display: flex; list-style: none; margin: 0; padding: 0;">
+        <div>
+            <ul>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">
-                        Home
-                    </a>
+                    <a href="#">Home</a>
                 </li>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">
-                        Prerequisites
-                    </a>
+                    <a href="#">Prerequisites</a>
                 </li>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">
-                        Modules
-                    </a>
+                    <a href="#">Modules</a>
                 </li>
                 <li>
-                    <a href="#" style="color: #fff; padding: 0 1em; text-decoration: none;">
-                        About
-                    </a>
+                    <a href="#">About</a>
                 </li>
             </ul>
         </div>
-    `
+    `,
+    styleUrls: ['./app-nav.component.css']
 })
 
 export class AppNavComponent {
