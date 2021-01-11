a0a53734518e81d3134d187b86a584447eb3eb20
Brian.Treese <brian.treese@socreate.it>
2021-01-10 22:00:57 -0800

    fix formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'saa-app',
   template: `
-    <header>
-      <h1>Styling Angular Applications</h1>
-      <em>For Modern Angular Applications</em>
-    </header>
-    <div>
-      <saa-app-nav></saa-app-nav>
-    </div>
+      <header>
+          <h1>Styling Angular Applications</h1>
+          <em>For Modern Angular Applications</em>
+      </header>
+      <div>
+          <saa-app-nav></saa-app-nav>
+      </div>
   `
 })
 
