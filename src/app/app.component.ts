3f1aac1d3c03a68c9aea97d91a070fd5ef31af84
Brian.Treese <brian.treese@socreate.it>
2021-01-10 21:59:58 -0800

    fix formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,29 +4,29 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'saa-app',
   encapsulation: ViewEncapsulation.None,
   template: `
-    <header>
-      <h1>Styling Angular Applications</h1>
-      <em>For Modern Angular Applications</em>
-    </header>
-    <div>
-      <saa-app-nav></saa-app-nav>
-    </div>
+      <header>
+          <h1>Styling Angular Applications</h1>
+          <em>For Modern Angular Applications</em>
+      </header>
+      <div>
+          <saa-app-nav></saa-app-nav>
+      </div>
   `,
   styles: [`
 
-    body {
-      color: #666;
-      font-family: arial, sans-serif;
-      margin: 0;
-      padding: 1.5em;
-    }
+      body {
+          color: #666;
+          font-family: arial, sans-serif;
+          margin: 0;
+          padding: 1.5em;
+      }
 
-    h1 {
-      color: #2A9FBC;
-      font-size: 200%;
-      line-height: 1;
-      margin: 0.5em 0 0.25em;
-    }
+      h1 {
+          color: #2A9FBC;
+          font-size: 200%;
+          line-height: 1;
+          margin: 0.5em 0 0.25em;
+      }
 
   `]
 })
