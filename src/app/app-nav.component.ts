00cf5aa91e13a769c7ccf345409ea8982fde3fe5
Brian.Treese <brian.treese@socreate.it>
2021-01-10 21:56:27 -0800

    remove view encapsulation

--- src/app/app-nav.component.ts
+++ src/app/app-nav.component.ts
@@ -3,7 +3,33 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'saa-app-nav',
     template: `
-        <link rel="stylesheet" href="app/app-nav.component.css" />
+        <style>
+            
+            div {
+                background: #2A9FBC;
+                border-radius: 0.5em;
+                margin: 1.5em 0;
+                padding: 1em 1.2em;
+            }
+            
+            ul {
+                display: flex;
+                list-style: none;
+                margin: 0;
+                padding: 0;
+            }
+            
+            a {
+                color: #fff;
+                padding: 0 1em;
+                text-decoration: none;
+            }
+
+            a:hover {
+                text-decoration: underline;
+            }
+
+        </style>
         <div>
             <ul>
                 <li>
