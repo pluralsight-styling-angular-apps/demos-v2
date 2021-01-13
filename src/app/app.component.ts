83a6933a4a0f8f71873131541715231d5749f716
Brian.Treese <brian.treese@socreate.it>
2021-01-12 23:17:02 -0800

    add proper path for template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'saa-app',
   encapsulation: ViewEncapsulation.ShadowDom,
-  templateUrl: 'app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
