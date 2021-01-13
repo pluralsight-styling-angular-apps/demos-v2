fbafeb31a8ce83df597a72ebf2677e7c8656e62f
Brian.Treese <brian.treese@socreate.it>
2021-01-12 23:19:49 -0800

    remove view encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'saa-app',
-  encapsulation: ViewEncapsulation.ShadowDom,
   template: `
       <header>
           <h1>Styling Angular Applications</h1>
