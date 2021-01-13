0dfd3fd0629c4615219313ea9fa91d6f9204a3e7
Brian.Treese <brian.treese@socreate.it>
2021-01-12 21:33:30 -0800

    clean up promo component code

--- src/app/promo/promo.component.ts
+++ src/app/promo/promo.component.ts
@@ -2,21 +2,18 @@ import { Component, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'saa-promo',
-    templateUrl: 'promo.component.html',
-    styleUrls: ['promo.component.scss']
+    templateUrl: './promo.component.html',
+    styleUrls: ['./promo.component.scss']
 })
 
 export class PromoComponent {
-    themeClassNames;
+    themeClassNames: string[] = [];
     isColor01 = false;
 
     constructor(private hostRef: ElementRef) {}
 
     ngAfterContentInit() {
         this.themeClassNames = this.hostRef.nativeElement.className.split(' ');
-        if (this.themeClassNames.indexOf('color--01') > -1) {
-            this.isColor01 = true;
-        }
+        this.isColor01 = this.themeClassNames.indexOf('color--01') > -1;
     }
-
 }
