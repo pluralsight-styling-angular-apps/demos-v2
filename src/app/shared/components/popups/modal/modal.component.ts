f12d6da75649e8b318938e0a5c565805b7c10e4b
Brian.Treese <brian.treese@socreate.it>
2021-01-12 22:38:29 -0800

    clean up pop ups components code

--- src/app/shared/components/popups/modal/modal.component.ts
+++ src/app/shared/components/popups/modal/modal.component.ts
@@ -2,19 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'saa-modal',
-  templateUrl: 'modal.component.html',
-  styleUrls: ['modal.component.scss']
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss']
 })
 
 export class ModalComponent {
     isOpen = false;
 
     toggleOpenClose() {
-        if (!this.isOpen) {
-            this.isOpen = true;
-        } else {
-            this.isOpen = false;
-        }
+        this.isOpen = !this.isOpen;
     }
-
 }
--- src/app/shared/components/popups/tooltip/tooltip.component.ts
+++ src/app/shared/components/popups/tooltip/tooltip.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'saa-tooltip',
-    templateUrl: 'tooltip.component.html',
-    styleUrls: ['tooltip.component.scss']
+    templateUrl: './tooltip.component.html',
+    styleUrls: ['./tooltip.component.scss']
 })
 
 export class ToolTipComponent { }
