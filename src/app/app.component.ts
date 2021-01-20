c5aa7927313679bb40a8c2edd77e009dfe768c02
Brian.Treese <brian.treese@socreate.it>
2021-01-19 23:06:50 -0800

    add module-03-19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'saa-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./scss/globals.scss', './scss/header.scss', './scss/content.scss', './scss/footer.scss']
+    styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
