5ab1a7e7c083972ce8cdad8ebda046249e7c791e
Brian.Treese <brian.treese@socreate.it>
2021-02-16 21:36:48 -0800

    convert slot attributes to directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,10 @@ import { AccordionPaneComponent }   from './shared/components/content/accordion/
 import { TabsComponent }            from './shared/components/content/tabs/tabs.component';
 import { TabsPaneComponent }        from './shared/components/content/tabs/tabs-pane/tabs-pane.component';
 import { ThumbnailListComponent }   from './shared/components/content/thumbnail-list/thumbnail-list.component';
-import { MessageComponent }         from './shared/components/content/message/message.component';
+import {
+  MessageComponent,
+  MessageTitleDirective,
+  MessageContentDirective }         from './shared/components/content/message/message.component';
 
 // Navigation Components
 import { NavBarPrimaryComponent }   from './shared/components/navigation/navbar-primary/navbar-primary.component';
@@ -30,6 +33,8 @@ import { ToolTipComponent }         from './shared/components/popups/tooltip/too
       TabsComponent,
       TabsPaneComponent,
       MessageComponent,
+      MessageTitleDirective,
+      MessageContentDirective,
       ThumbnailListComponent,
       NavBarPrimaryComponent,
       NavBarSecondaryComponent,
