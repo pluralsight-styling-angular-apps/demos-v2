2ff4708dbb74f8b56408db08d6f79f247efb63ee
Brian.Treese <brian.treese@socreate.it>
2021-03-08 20:56:59 -0800

    add view encapsulation code commented out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'saa-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    // encapsulation: ViewEncapsulation.None,
+    // styleUrls: [
+    //     './shared/scss/root/normalize.scss',
+    //     './shared/scss/root/root.scss',
+    //     './shared/scss/root/links.scss',
+    //     './shared/scss/layout/app-footer.scss',
+    //     './shared/scss/layout/app-header.scss',
+    //     './shared/scss/layout/layout.scss',
+    //     './shared/scss/typography/headers.scss',
+    //     './shared/scss/typography/paragraphs.scss'
+    // ]
 })
 
 export class AppComponent {
